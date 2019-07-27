1db961af575335cdf1a51dcbe092df0fbb769e6f
winestone <stonewine@hotmail.com>
2019-07-27 10:32:12 +0000

    Add add some orm db code

--- proj/src/backend/index.ts
+++ proj/src/backend/index.ts
@@ -1,113 +1,116 @@
 // lib/app.ts
 import express from "express";
 import path from "path";
-import { v4 as uuid } from "uuid";
+import typeorm from "typeorm";
+import {v4 as uuid} from "uuid";
+import "reflect-metadata";
 
 import * as common from "../common/common";
+import {Food, User} from "./db";
 
-// Create a new express application instance
-const app: express.Application = express();
-
-type FoodLocationWithTime = common.AddFoodLocation & { time: Date };
-
-interface User {
-  username: string;
-  password: string;
-  business_name: string;
-  address: string;
-}
 interface Session {
   session_uuid: string;
-  username: string;
+  id: number;
   session_start: Date;
 }
 
-const users_by_username: { [username: string]: User } = {};
-const sessions: { [session_uuid: string]: Session } = {};
-const food_locations: FoodLocationWithTime[] = [];
+typeorm.createConnection({
+  type: "sqlite",
+  database: "db.sqlite",
+  entities: [Food, User],
+  synchronize: true,
+  logging: false
+}).then(async connection => {
 
-function isLoggedIn(req: express.Request): boolean {
-  return sessions[req.cookies.session_uuid] !== undefined;
-}
-// Returns whether registration was successful
-function registerUser(user: User): boolean {
-  if ( users_by_username[user.username] !== undefined ) {
-    return false;
+  // Create a new express application instance
+  const app: express.Application = express();
+
+  const sessions: { [session_uuid: string]: Session } = {};
+
+  function isLoggedIn(req: express.Request): boolean {
+    return sessions[req.cookies.session_uuid] !== undefined;
   }
-  users_by_username[user.username] = user;
-  return true;
-}
-
-// return session uuid
-function loginUser(username: string, password: string): string | null {
-  const user = users_by_username[username];
-  if (user === undefined || user.password !== password) return null;
-  const session_uuid = uuid();
-  sessions[session_uuid] = {
-    session_uuid,
-    username,
-    session_start: new Date(),
-  };
-  return session_uuid;
-}
-function logoutUser(session_uuid: string) {
-  delete sessions[session_uuid];
-}
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/add_food", (req, res) => {
-  const x: FoodLocationWithTime = req.body;
-  console.log("/add_food received", req.body);
-  x.time = new Date();
-  food_locations.push(x);
-  res.json(true);
-});
-
-function removeOldFoods() {
-  const curr_time = new Date();
-  curr_time.setHours(curr_time.getHours() + 4);
-  while (0 < food_locations.length && curr_time < food_locations[0].time) {
-    food_locations.shift();
+  // Returns whether registration was successful
+  function registerUser(user: User): boolean {
+    if ( users_by_username[user.username] !== undefined ) {
+      return false;
+    }
+    users_by_username[user.username] = user;
+    return true;
   }
-}
 
-app.get("/get_food", (req, res) => {
-  removeOldFoods();
-  res.json(food_locations);
-});
+  // return session uuid
+  function loginUser(username: string, password: string): string | null {
+    const user = users_by_username[username];
+    if (user === undefined || user.password !== password) return null;
+    const session_uuid = uuid();
+    sessions[session_uuid] = {
+      session_uuid,
+      username,
+      session_start: new Date(),
+    };
+    return session_uuid;
+  }
+  function logoutUser(session_uuid: string) {
+    delete sessions[session_uuid];
+  }
 
-app.use("/static", express.static(path.join(__dirname, "../../static")));
-app.use("/dist", express.static(path.join(__dirname, "../../dist")));
+  app.use(express.json());
 
-app.post("/login", (req, res) => {
-  if (typeof(req.body) === "object" && typeof(req.body.username) === "string" && typeof(req.body.password) === "string") {
-    const session_uuid = loginUser(req.body.username, req.body.password);
-    if (session_uuid !== null) {
-      res.cookie("session_uuid", session_uuid);
-      res.json(true);
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.post("/add_food", (req, res) => {
+    const x: FoodLocationWithTime = req.body;
+    console.log("/add_food received", req.body);
+    x.time = new Date();
+    food_locations.push(x);
+    res.json(true);
+  });
+
+  function removeOldFoods() {
+    const curr_time = new Date();
+    curr_time.setHours(curr_time.getHours() + 4);
+    while (0 < food_locations.length && curr_time < food_locations[0].time) {
+      food_locations.shift();
     }
   }
-  res.json(false);
-});
 
-app.post("/register", (req, res) => {
-  if (typeof(req.body) === "object" && typeof(req.body.username) === "string" && typeof(req.body.password) == "string" &&
-    typeof(req.body.business_name) === "string" && typeof(req.body.address) === "string") {
-    res.json(registerUser(req.body));
-    return;
-  }
-  res.json(false);
-});
+  app.get("/get_food", (req, res) => {
+    removeOldFoods();
+    res.json(food_locations);
+  });
 
-app.get("/logout", (req, res) => {
-  logoutUser(req.cookies.session_uuid);
-});
+  app.use("/static", express.static(path.join(__dirname, "../../static")));
+  app.use("/dist", express.static(path.join(__dirname, "../../dist")));
 
-app.listen(8000, () => {
-  console.log("Example app listening on port 8000!");
-});
+  app.post("/login", (req, res) => {
+    if (typeof(req.body) === "object" && typeof(req.body.username) === "string" && typeof(req.body.password) === "string") {
+      const session_uuid = loginUser(req.body.username, req.body.password);
+      if (session_uuid !== null) {
+        res.cookie("session_uuid", session_uuid);
+        res.json(true);
+      }
+    }
+    res.json(false);
+  });
+
+  app.post("/register", (req, res) => {
+    if (typeof(req.body) === "object" && typeof(req.body.username) === "string" && typeof(req.body.password) == "string" &&
+      typeof(req.body.business_name) === "string" && typeof(req.body.address) === "string") {
+      res.json(registerUser(req.body));
+      return;
+    }
+    res.json(false);
+  });
+
+  app.get("/logout", (req, res) => {
+    logoutUser(req.cookies.session_uuid);
+  });
+
+  app.listen(8000, () => {
+    console.log("Example app listening on port 8000!");
+  });
+
+}).catch(error => console.log(error));
