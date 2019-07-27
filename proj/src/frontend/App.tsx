a6bdb188f0113a50ffb2dfcdab680a17319e4dd4
Declan Warn <d.warn@student.unsw.edu.au>
2019-07-27 22:45:44 +1000

    basic routing

--- proj/src/frontend/App.tsx
+++ proj/src/frontend/App.tsx
@@ -4,15 +4,19 @@ import LocationForm from "./LocationForm";
 import LocationList from "./LocationList";
 import LocationMap from "./LocationMap";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 interface AppProps { };
 
 interface AppState {
     showForm: boolean;
+    signedIn: boolean;
+    content: AppContent;
 };
+
+type AppContent =
+    | "list"
+    | "signin"
+    | "signup"
+    | "donate";
 
 class App extends React.Component<AppProps, AppState> {
     constructor(props: any) {
@@ -20,26 +24,89 @@ class App extends React.Component<AppProps, AppState> {
 
         this.state = {
             showForm: false,
+            signedIn: false,
+            content: "list",
         };
+
+        this.appNav = this.appNav.bind(this);
+    }
+
+    appNav() {
+        if (this.state.signedIn) {
+            return (
+                <ul>
+                    <li
+                        onClick={() => this.setState({ content: "donate" })}
+                    >
+                        Donate
+                    </li>
+                    <li
+                        onClick={() => this.setState({ signedIn: false })}
+                    >
+                        Sign Out
+                    </li>
+                </ul>
+            );
+        } else {
+            return (
+                <ul>
+                    <li
+                        onClick={() => this.setState({ signedIn: true })}
+                    >
+                        Sign In
+                    </li>
+                    <li
+                        onClick={() => this.setState({ content: "signup" })}
+                    >
+                        Sign Up
+                    </li>
+                </ul>
+            );
+        }
+    }
+
+    appBody() {
+        switch (this.state.content) {
+            case "list":
+                return (
+                    <LocationList />
+                );
+
+            case "signin":
+                return (
+                    <span>TODO: signin</span>
+                );
+
+            case "signup":
+                return (
+                    <span>TODO: signup</span>
+                );
+
+            case "donate":
+                return (
+                    <span>TODO: donate</span>
+                );
+
+            default:
+                console.log("UNKNOWN CONTENT TYPE:", this.state.content);
+                return null;
+        }
     }
 
     render() {
         return (
             <main>
-                <LocationMap/>
+                <LocationMap />
                 <aside>
                     <nav>
-                        <ul>
-                            <li>Sign In</li>
-                            <li>Sign Up</li>
-                        </ul>
+                        {this.appNav()}
                     </nav>
                     <div>
-                        <LocationList />
+                        {this.appBody()}
                     </div>
                 </aside>
                 {this.state.showForm && <LocationForm />}
-            </main>
+            </main >
         );
     }
 }
