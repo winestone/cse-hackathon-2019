c6b5c4185e2856b7fe05753bba3dcf527dcc955f
winestone <stonewine@hotmail.com>
2019-07-27 09:53:27 +0000

    Define the v2 api

--- proj/src/common/common.ts
+++ proj/src/common/common.ts
@@ -1,6 +1,58 @@
 
-export interface AddFoodLocation {
-  name: string;
-  address: string;
-  description: string;
+export interface Location {
+  longitude: string,
+  latitude: string,
 }
+
+export interface UserAndPass {
+  username: string;
+  password: string;
+}
+
+export interface User {
+  username: string;
+  password: string;
+  business_name: string;
+  location: Location;
+}
+
+export interface AddFoodLocation {
+  description: string;
+  image: string; // base64 encoded png/jpeg?
+}
+
+export interface FoodLocation {
+  id: number;
+  business_name: string;
+  location: Location;
+  description: string;
+  image: string; // base64 encoded png/jpeg?
+}
+
+export interface FoodCancel {
+  id: number;
+}
+
+/*
+  POST /register
+    - body: User
+    - returns: boolean, true if successful
+  POST /login
+    - body: UserAndPass
+    - returns: boolean, true if successful, also sets the 'session_uuid' cookie
+  POST /logout
+    - body: ignored
+    - returns: nothing
+  POST /food
+    - body: AddFoodLocation
+    - returns: nothing
+  GET  /food
+    - body: ignored
+    - returns: array of FoodLocation
+  GET  /food/self
+    - body: ignored
+    - returns: array of FoodLocation belonging to yourself
+  POST /food/cancel
+    - body: FoodCancel
+    - returns: nothing
+*/
